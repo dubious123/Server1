9c98420e185bc725a85cd184acca8a124b0fafa0
J.Han <dubious123@hanyang.ac.kr>
2022-04-10 16:58:24 +0900

    Initial commit

---
+++ DummyClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Sockets;
+using System;
+using System.Net;
+using System.Threading;
+
+namespace DummyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            string host = Dns.GetHostName();
+            var ip = Dns.GetHostEntry(host);
+            var ipAddress = ip.AddressList[0];
+            var endPoint = new IPEndPoint(ipAddress, 1234);
+            byte[] fromServer = new byte[1];
+            Console.WriteLine(fromServer[0]);
+            for (; true; )
+            {
+                Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                socket.Connect(ipAddress, 1234);
+                socket.Send(new byte[] { 1 });
+                Thread.Sleep(250);
+                socket.Receive(fromServer);
+                Console.WriteLine(fromServer[0]);
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string host = Dns.GetHostName();
+            var ip = Dns.GetHostEntry(host);
+            var ipAddress = ip.AddressList[0];
+            var endPoint = new IPEndPoint(ipAddress, 1234);
+            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(endPoint);
+            socket.Listen();
+            byte[] recieveContent = new byte[1];
+            while (true)
+            {
+                var s = socket.Accept();
+                if (s != null)
+                    s.Receive(recieveContent);
+
+                Console.WriteLine(recieveContent[0]);
+
+                s.Send(new byte[1] { 1 });
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
